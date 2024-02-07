72a5c01ddc9b9cd60b7f5c077488d513d75a9789
Anujith <legendarymmatko@gmail.com>
2024-02-07 23:28:30 +0530

    created sample working api for sign-up

--- server/index.js
+++ server/index.js
@@ -1,20 +1,38 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
+import { User } from "./models/userModel.js";
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-
-
-mongoose.connect('mongodb://127.0.0.1:27017/BlogAppReact')
-.then(() => {
-  console.log('server connected to database');
-  app.listen(PORT, () => {
-    console.log(`Server running at Port:${PORT}`);
+app.post("/sign-up", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = User({
+    email: email,
+    password: password
   })
-})
-.catch((error) => {
-  console.log(error);
-})
+  newUser.save()
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  res.send({message:"success"})
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/BlogAppReact")
+  .then(() => {
+    console.log("server connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server running at Port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
